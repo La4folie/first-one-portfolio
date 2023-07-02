0b342e88b1962f2b43ffc6153dd156c37392a9c4
Alex Chumarov <alosha.chumarov@gmail.com>
2023-07-02 16:34:35 +0200

    Feature

--- src/contact/contact.tsx
+++ src/contact/contact.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import s from './contact.module.css'
 import sc from './../common/styles/Container.module.css';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
-    return (
-        <div className={s.contact}>
-            <div className={`${sc.container} ${s.contactContainer}`}>
-                <h2 className={s.contactTitle}>Let's Work <span className={s.greenSpan}>Together!</span></h2>
-                <form className={s.basicBlock}>
-                    <p>FULL NAME*</p>
-                    <input className={s.inputForm}/>
-                    <p>email*</p>
-                    <input className={s.inputForm}/>
-                    <p>Phone(Optional)</p>
-                    <input className={s.inputForm}/>
-                    <p>MESSAGE</p>
-                    <input title={'Write your message here'} className={s.inputForm}/>
-                    <div className={s.button}>
-                        <button type='submit'>Send Message</button>
-                        </div>
-                </form>
-            </div>
-        </div>
-    );
-};
 
-export default Contact;
+
+        const sendEmail = (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+
+            emailjs
+                .sendForm('service_e9uf3wr', 'template_andn675', event.currentTarget, 'I26Z32QctDuyL20VB')
+                .then((res) => {
+                    console.log(res.text);
+                })
+                .catch((err) => {
+                    console.log(err.text);
+                });
+        }
+
+            return (
+                <div className={s.contact}>
+                    <div className={`${sc.container} ${s.contactContainer}`}>
+                        <h2 className={s.contactTitle}>Let's Work <span className={s.greenSpan}>Together!</span></h2>
+                        <form className={s.basicBlock} onSubmit={sendEmail}>
+                            <p>FULL NAME*</p>
+                            <input className={s.inputForm} type='text' name='from_name'/>
+                            <p>email*</p>
+                            <input className={s.inputForm} type='email' name='user_email'/>
+                            <p>MESSAGE</p>
+                            <textarea className={s.inputForm} name='message'/>
+                            <div className={s.button}>
+                                <button type='submit' value='send'>Send Message</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )
+        };
+
+        export default Contact;
--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -4,12 +4,14 @@ import s from './Nav.module.css';
 const Nav = () => {
     return (
         <div className={s.nav}>
-            <a href={""}>Introduce</a>
-            <a href={""}>About</a>
-            <a href={""}>Skills</a>
-            <a href={""}>Resume</a>
-            <a href={""}>Portfolio</a>
-            <a href={""}>Contact</a>
+            <ul>
+                <a href={""}>Introduce</a>
+                <a href={""}>About</a>
+                <a href={""}>Skills</a>
+                <a href={""}>Resume</a>
+                <a href={""}>Portfolio</a>
+                <a href={""}>Contact</a>
+            </ul>
         </div>
     );
 }
